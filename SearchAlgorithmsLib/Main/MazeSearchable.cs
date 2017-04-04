dd1492d955279b8259456d2b467957945f3fa7ba
ayelet ehrman <ayelet099@gmail.com>
2017-04-04 18:05:07 +0300

    tue.

--- SearchAlgorithmsLib/Main/MazeSearchable.cs
+++ SearchAlgorithmsLib/Main/MazeSearchable.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 
-using System.Text;
 using MazeLib;
-
-namespace SearchAlgorithmsLib
+using SearchAlgorithmsLib;
+namespace Main
 {
     public class MazeSearchable : ISearchable<Position>
     {
