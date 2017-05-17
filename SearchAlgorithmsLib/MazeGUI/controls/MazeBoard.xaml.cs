d25363afe7896176f01d8bbb75a439313a09f908
ayelet ehrman <ayelet099@gmail.com>
2017-05-17 19:04:41 +0300

    17/5 ex2

---
+++ SearchAlgorithmsLib/MazeGUI/controls/MazeBoard.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MazeGUI.controls
+{
+    /// <summary>
+    /// Interaction logic for MazeBoard.xaml
+    /// </summary>
+    public partial class MazeBoard : UserControl
+    {
+        public MazeBoard()
+        {
+            InitializeComponent();
+        }
+        public int Rows
+        {
+            get { return (int)GetValue(RowsProperty); }
+            set { SetValue(RowsProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for Rows.
+        // This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RowsProperty =
+         DependencyProperty.Register("Rows", typeof(int), typeof(MazeBoard),                                     new PropertyMetadata(0));
+    }
+}
